5c11753a690655b21b07b1e8b4cb50afce06f297
admin100-sm <andrialousada20@gmail.com>
2026-09-13 00:19:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 console.log("Firebase ligado com sucesso!");
+document.getElementById("estadoFirebase").textContent =
+    "Firebase ligado com sucesso!";
