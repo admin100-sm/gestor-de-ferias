81b8b14f15aedf7d0771336390e1c1900c1d2e5e
admin100-sm <andrialousada20@gmail.com>
2026-09-13 23:06:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import {
+    getFirestore,
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCbzt8YzCAwZ2_OyQ1_E_TRSE0uLp2_PrE",
