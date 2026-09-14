9ef96d23d787376bb3530b184d00000e034b00de
admin100-sm <andrialousada20@gmail.com>
2026-09-14 23:22:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
+const auth = getAuth(app);
+
 const estadoFirebase = document.getElementById("estadoFirebase");
 
 if (estadoFirebase) {
