7d1a52030876ecb85bda773495e8580041fc07d4
admin100-sm <andrialousada20@gmail.com>
2026-09-14 23:21:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import {
+    getAuth,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import {
     getFirestore,
     collection,
     addDoc
